65dc6d02f13d8d245bff9e5b1a9aa41044c2c7e1
Damayanti Bhuyan <dbhuyan@microsoft.com>
2024-02-01 11:01:24 +0100

    added

---
+++ src/WebApis/ComplianceWebApi/builddocker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+imageName="ComplianceWebApi"
+tag="beta2"
+registry="neptuneimages.azurecr.io"
+
+echo "Login to Azure Container Registry"
+accessToken=$(az acr login --name $registry --expose-token --output tsv --query accessToken)
+docker login $registry --username 00000000-0000-0000-0000-000000000000 --password $accessToken
+
+echo "Building Images with Tag '${imageName}:${tag}'"
+docker build -t ${registry}/${imageName}:${tag} .
+
+echo "Pushing to '$registry'"
+docker push ${registry}/${imageName}:${tag}
