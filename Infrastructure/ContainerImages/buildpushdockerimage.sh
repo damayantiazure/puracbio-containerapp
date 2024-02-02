f3cfe505c4f785a2d8e4690d6dd5e4e018f2ca14
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-02-02 15:45:09 +0000

    Updated buildpushdockerimage.sh

--- Infrastructure/ContainerImages/buildpushdockerimage.sh
+++ Infrastructure/ContainerImages/buildpushdockerimage.sh
@@ -11,11 +11,11 @@ echo $APP_NAME
 echo $APP_ENV
 
 echo "Login to Azure Container Registry"
-accessToken=$(az acr login --name $(registry) --expose-token --output tsv --query accessToken)
-docker login $(registry) --username 00000000-0000-0000-0000-000000000000 --password $accessToken
+accessToken=$(az acr login --name $registry --expose-token --output tsv --query accessToken)
+docker login $registry --username 00000000-0000-0000-0000-000000000000 --password $accessToken
       
 echo "Building Images with Tag '${imageName}:${tag}'"
-docker build -t $(registry)/$(imageName):$(tag) -f ./src/WebApis/ComplianceWebApi/Dockerfile .
+docker build -t $registry/$imageName:$tag -f ./src/WebApis/ComplianceWebApi/Dockerfile .
       
 echo "Pushing to '$registry'"
-docker push $(registry)/$(imageName):$(tag)
+docker push $registry/$imageName:$tag
