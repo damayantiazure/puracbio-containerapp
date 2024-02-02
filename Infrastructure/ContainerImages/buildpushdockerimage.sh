75fdbe5319c89bdb4ecdba48dbead95170e441b2
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-02-02 15:55:32 +0000

    Updated buildpushdockerimage.sh

--- Infrastructure/ContainerImages/buildpushdockerimage.sh
+++ Infrastructure/ContainerImages/buildpushdockerimage.sh
@@ -8,11 +8,8 @@ echo $registry
 echo $imageName
 echo $tag
 
-URL=$registry
-CLEAN_URL=$(echo $URL | tr -d '\r')
-
 echo "Login to Azure Container Registry"
-accessToken=$(az acr login --name $URL --expose-token --output tsv --query accessToken)
+accessToken=$(az acr login --name $registry --expose-token --output tsv --query accessToken)
 docker login $registry --username 00000000-0000-0000-0000-000000000000 --password $accessToken
       
 echo "Building Images with Tag '${imageName}:${tag}'"
