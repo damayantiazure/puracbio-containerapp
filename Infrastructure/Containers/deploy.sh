5e46d9f0bd815c8074f45126fdb6b336f4c31f8a
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-02-03 12:22:54 +0000

    Updated deploy.sh

--- Infrastructure/Containers/deploy.sh
+++ Infrastructure/Containers/deploy.sh
@@ -5,9 +5,11 @@ export resourceGroupName=$resourceGroupName
 export location=$location
 export APP_NAME=$APP_NAME
 export APP_ENV=$APP_ENV
-export TAG_NAME=$tag
-export TAG_NAME=$tag
+export containerRegistryName=$containerRegistryName
+export tagNameE=$tag
 export imageName=$imageName
+export uamiName=$uamiName
+export appInsightName=$appInsightName
 
 echo "Starting deploying the app provisioning..."
 
