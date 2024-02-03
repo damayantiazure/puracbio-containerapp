0d44d0a44ed288337db68e6a0611cc7bcf858493
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-02-03 11:12:20 +0000

    Updated buildpushdockerimage.sh

--- Infrastructure/ContainerImages/buildpushdockerimage.sh
+++ Infrastructure/ContainerImages/buildpushdockerimage.sh
@@ -1,15 +1,6 @@
-
-export APP_NAME="solar201"
-export APP_ENV="dev"
-export imageName="compliancewebapi"
-export tag="567"
-export registry="$(APP_NAME)contregistry$(APP_ENV).azurecr.io"
-export imageRepository="compliancewebapi"
-
-echo $registry
 echo "Login to Azure Container Registry"
-accessToken=$(az acr login --name $registry --expose-token --output tsv --query accessToken)
-docker login "$registry" --username 00000000-0000-0000-0000-000000000000 --password $accessToken
+accessToken=$(az acr login --name ${registry} --expose-token --output tsv --query accessToken)
+docker login ${registry} --username 00000000-0000-0000-0000-000000000000 --password $accessToken
 
 echo "Building Images with Tag '${imageName}:${tag}'"
 docker build -t ${registry}/${imageName}:${tag} .
