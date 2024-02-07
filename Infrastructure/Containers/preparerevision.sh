e3b5e6b0c2c54f9323a2da97f1e87ad5e9f490c0
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-02-07 13:08:24 +0000

    Added preparerevision.sh

---
+++ Infrastructure/Containers/preparerevision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+COMMITHASH=$1
+FileName=$2
+
+echo "Starting script...Commit Hash received $COMMITHASH and file name $FileName"
+az config set extension.use_dynamic_install=yes_without_prompt
+az extension add -n containerapp
+
+nextRevisionName="xeniel-frontend--${COMMITHASH:0:10}"
+previousRevisionName=$(az containerapp revision list -n xeniel-frontend -g xeniel --query '[0].name')
+
+prevNameWithoutQuites=$(echo $previousRevisionName | tr -d "\"")        # using sed echo $pname | sed "s/\"//g"
+echo 'Previous revision name: ' $prevNameWithoutQuites
+echo 'Next revision name: ' $nextRevisionName
+
+sed -i "s/PREV/$prevNameWithoutQuites/g" ${PWD}/Infrastructure/$FileName 
+sed -i "s/NEXT/$nextRevisionName/g" ${PWD}/Infrastructure/$FileName 
+
+
+cat ${PWD}/Infrastructure/$FileName
