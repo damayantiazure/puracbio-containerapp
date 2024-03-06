b37f2a50286a011b2630030c3c882954652ec820
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-06 13:15:40 +0000

    Updated builddocker.sh

--- src/WebApis/ComplianceWebApi/builddocker.sh
+++ src/WebApis/ComplianceWebApi/builddocker.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# imageName="ComplianceWebApi"
-# tag="beta2"
-# registry="neptuneimages.azurecr.io"
-
-imageName=${imageName}
-tag=${tag}
-registry=$(registry)
-# var imageName = readEnvironmentVariable('imageName')
-# var tag = readEnvironmentVariable('tag')
-# var tag = readEnvironmentVariable('registry')
-
-
 echo "Login to Azure Container Registry"
 accessToken=$(az acr login --name $registry --expose-token --output tsv --query accessToken)
 docker login $registry --username 00000000-0000-0000-0000-000000000000 --password $accessToken
