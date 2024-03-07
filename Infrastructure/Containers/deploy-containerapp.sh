0140f3306fd1746aa849cf42de910687d5f1b424
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-07 07:53:36 +0000

    Updated deploy-containerapp.sh

--- Infrastructure/Containers/deploy-containerapp.sh
+++ Infrastructure/Containers/deploy-containerapp.sh
@@ -11,7 +11,11 @@
 # export uamiName=$uamiName
 # export appInsightName=$appInsightName
 
+echo $tag
+echo $imageName
+echo $resourceGroupName
+
 echo "Starting deploying the app provisioning..."
 
 echo "Deploying app Bicep file..."
-az deployment group create --resource-group 'APIM-DEVOPS' --template-file 'Infrastructure/Containers/app.bicep' --parameters 'Infrastructure/Containers/app.bicepparam'
+az deployment group create --resource-group $resourceGroupName --template-file 'Infrastructure/Containers/app.bicep' --parameters 'Infrastructure/Containers/app.bicepparam'
