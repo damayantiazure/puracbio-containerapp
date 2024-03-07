c6e3cfcf213e1f4c03eb95b6f544b1667e84d1d5
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-07 11:47:45 +0000

    Added 2 files to /Infrastructure/scripts

---
+++ Infrastructure/scripts/create-infra (1).sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# export resourceGroupName=$resourceGroupName
+# export location=$location
+# export APP_NAME=$APP_NAME
+# export APP_ENV=$APP_ENV
+
+echo "Starting Infrastructure provisioning..."
+echo $resourceGroupName
+
+echo "Creating resource group..."
+az group create --name $resourceGroupName --location $location
+
+echo "Deploying main Bicep file..."
+#az deployment group create --confirm-with-what-if --resource-group $resourceGroupName --template-file main.bicep  --parameters main.bicepparam
+
+az deployment group create --resource-group $resourceGroupName --template-file Infrastructure/Platform/main.bicep  --parameters Infrastructure/Platform/main.bicepparam
---
+++ Infrastructure/scripts/deploy-containerapp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# export resourceGroupName=$resourceGroupName
+# export location=$location
+# export APP_NAME=$APP_NAME
+# export APP_ENV=$APP_ENV
+# export containerRegistryName=$containerRegistryName
+# export tagNameE=$tag
+# export imageName=$imageName
+# export uamiName=$uamiName
+# export appInsightName=$appInsightName
+
+echo $tag
+echo $imageName
+echo $resourceGroupName
+
+echo "Starting deploying the app provisioning..."
+
+echo "Deploying app Bicep file..."
+az deployment group create --resource-group $resourceGroupName --template-file 'Infrastructure/Containers/app.bicep' --parameters 'Infrastructure/Containers/app.bicepparam'
