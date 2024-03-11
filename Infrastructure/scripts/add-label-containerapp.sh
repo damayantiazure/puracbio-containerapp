73b2a84a51734a58c3980dded52992a823bf7ca6
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-11 12:25:29 +0000

    Updated add-label-containerapp.sh

--- Infrastructure/scripts/add-label-containerapp.sh
+++ Infrastructure/scripts/add-label-containerapp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Add Label to Container app"
-labelgreen="Green"
 
 previousRevisionName=$(az containerapp revision list -n $imageName -g $resourceGroupName --query '[0].name' | tr -d '"')     
 latestRevisionName="$imageName--$tag"
