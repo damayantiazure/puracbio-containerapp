62ab3972ba4cc0ebd312c0aa7ffc714cb639bc24
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-11 13:38:27 +0000

    Updated containerapp-healthcheck.sh

--- Infrastructure/scripts/containerapp-healthcheck.sh
+++ Infrastructure/scripts/containerapp-healthcheck.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Waiting for the new revision to be created"
+sleep 60
+
 latestRevisionName=$(az containerapp revision list -n $imageName -g $resourceGroupName --query '[0].name' | tr -d '"')  
 
 echo "Health check for the newly deployed app"
