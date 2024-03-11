93cc20854c0f2829772935716f88e1ad02be4384
System Administrator <admin@MngEnv877982.onmicrosoft.com>
2024-03-11 09:44:03 +0000

    Added add-label-containerapp.sh

---
+++ Infrastructure/scripts/add-label-containerapp.sh
@@ -0,0 +1,14 @@
+
+
+previousRevisionName="$(az containerapp revision list -n $(imageName) -g $(resourceGroupName) --query '[0].name' | tr -d '"')"      
+ latestRevisionName="$(imageName)--$(tag)"
+ echo $previousRevisionName
+      echo $latestRevisionName
+            
+      #Update Container app with updated API
+      echo "Updating the Container APP $(imageName)"
+      az containerapp update --name $(imageName) --resource-group $(resourceGroupName) --image $(containerRegistryName).azurecr.io/$(imageName):$(tag) --revision-suffix $(tag) --set-env-vars REVISION_COMMIT_ID=$(tag)
+      
+      #give that revision a 'green' label
+      echo "Add a Label green $(imageName)"
+      az containerapp revision label add --name $(imageName) --resource-group $(resourceGroupName) --label $(labelgreen) --revision $latestRevisionName
