3505634d64804ffe51fc3d27c8973a1af26cda22
damayantiazure <92169356+damayantiazure@users.noreply.github.com>
2024-08-09 10:31:51 +0200

    Update create-infra.sh

--- Infrastructure/scripts/create-infra.sh
+++ Infrastructure/scripts/create-infra.sh
@@ -12,6 +12,4 @@ echo "Creating resource group..."
 az group create --name $resourceGroupName --location $location
 
 echo "Deploying main Bicep file..."
-#az deployment group create --confirm-with-what-if --resource-group $resourceGroupName --template-file main.bicep  --parameters main.bicepparam
-
 az deployment group create --resource-group $resourceGroupName --template-file Infrastructure/Platform/main.bicep  --parameters Infrastructure/Platform/main.bicepparam
